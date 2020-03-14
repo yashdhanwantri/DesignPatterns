810b187bb4bc3f005110ac425b5b5b134e460080
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-03-14 18:30:22 +0530

    Copy Constructor Demonstrating

--- Prototype/Program.cs
+++ Prototype/Program.cs
@@ -2,11 +2,68 @@
 
 namespace Prototype
 {
+    //Demonstrating the use of Copy Constructor.
+    /// <summary>
+    /// ICloneable is bad, it creates shallow copy and moreover, we have to implement the clone() method which returns us the cloned object.
+    /// Demonstrating copy constructor
+    /// </summary>
+    class Person
+    {
+        public string[] Names;
+        public Address Address;
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {String.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    class Address
+    {
+        public string HouseNumber;
+        public string City;
+
+        public Address(string houseNumber, string city)
+        {
+            HouseNumber = houseNumber;
+            City = city;
+        }
+
+        public Address(Address other)
+        {
+            HouseNumber = other.HouseNumber;
+            City = other.City;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(HouseNumber)}: {HouseNumber}, {nameof(City)}: {City}";
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var yash = new Person(new[] { "Yash", "Dhanwantri" }, 
+                new Address("House123", "Minnesota"));
+
+            var suzi = new Person(yash);
+            suzi.Address.HouseNumber = "House945";
+            suzi.Names = new[] { "Suzi", "Wolker" };
+            Console.WriteLine(yash);
+            Console.WriteLine(suzi);
+            Console.WriteLine("Verifying cloning");
+            Console.WriteLine(yash);
         }
     }
 }
