473e3298df715356cc1b65eabf2939670360d8d7
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-03-24 11:21:50 +0530

    Singleton Implementation with Lazy Loading

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Singleton
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase: IDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(()=> new SingletonDatabase());
+        public static SingletonDatabase Instance => instance.Value;
+        private SingletonDatabase()
+        {
+            Console.WriteLine("Initializing database");
+            capitals = File.ReadAllLines("Cities.txt")
+                .Batch(2)
+                .ToDictionary(
+                list => list.ElementAt(0).Trim(),
+                list => int.Parse(list.ElementAt(1))
+                );
+        }
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDatabase.Instance;
+            var city = "Delhi";
+            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+        }
+    }
+}
